8ac27e095693e78e3df64dc09df65c1a3cf4bfef
Oren Lindsey <67929847+Oren-Lindsey@users.noreply.github.com>
2022-03-12 20:04:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const axios = require('axios')
+const WasteOfSession = require("wasteof-client")
+const pass = process.env['password']
+const username = 'art'
+const apiKey = process.env['api-keyy']
+const imgbbUploader = require("imgbb-uploader");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send(`this is a webserver to ensure replit doesn't shut down the bot. <br> <a href="https://wasteof.money/@${username}">page for the bot</a>`));
+
+app.listen(port, () => console.log(`Example app listening`));
+
+async function postWorkOfArt() {
+  await getRandomArt(handleArt);
+}
+function handleArt(imageData, museumData) {
+  const postData = `<b>${museumData.artistDisplayName}.</b> <b>${museumData.title}</b> <i>${museumData.medium}, ${museumData.objectDate}</i> <i>${museumData.culture}.</i> <p>${museumData.creditLine}. ${museumData.objectURL}</p> ${imageData.img}`
+  let wasteof = new WasteOfSession(username, pass)
+  wasteof.login()
+    .then(data => {
+      wasteof.setBio(`This is a bot that posts a random work of art from the Metropolitan Museum Of Art 4 times a day. @jeffalo please don't ban me`)
+      wasteof.postAndLove(postData, null)
+      console.log('posted successfully')
+    })
+}
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+function getRandomArt(callback) {
+  axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects`)
+    .then(res => {
+      const ids = res.data.objectIDs
+      const randomId = ids[Math.floor(Math.random()*ids.length)];
+      axios.get(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${randomId}`)
+        .then(res => {
+          uploadImage(res.data, callback)
+        })
+    })
+    .catch(function(err) {
+      console.log(res.data)
+    })
+}
+function uploadImage(urlData, callback) {
+  if (urlData.primaryImage.length === 0) {
+    if (urlData.primaryImageSmall.length === 0) {
+      var response = {'img': '<p>(No image available)</p>'}
+      callback(response, urlData)
+    } else {
+      const options = {
+        apiKey: process.env['api-keyy'],
+        imageUrl: urlData.primaryImageSmall,
+      };
+      imgbbUploader(options)
+        .then((response) => {
+          response.img = `<img src="${response.url}">`
+          callback(response, urlData)
+        })
+        .catch((error) => console.log(error));
+    }
+  } else {
+    const options = {
+      apiKey: process.env['api-keyy'],
+      imageUrl: urlData.primaryImage,
+    };
+    imgbbUploader(options)
+      .then((response) => {
+        response.img = `<img src="${response.url}">`
+        callback(response, urlData)
+      })
+      .catch((error) => console.log(error));
+  }
+}
+postWorkOfArt()
+setInterval(postWorkOfArt, 21600000);
